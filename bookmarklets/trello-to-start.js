683e9d886a520eb12887fbce9835bf3e735a86d6
calucido <calucido@users.noreply.github.com>
2017-09-03 21:26:33 -0400

    more specific search results; single quotes

--- bookmarklets/trello-to-start.js
+++ bookmarklets/trello-to-start.js
@@ -1,6 +1,6 @@
-if (window.location.pathname.match(/\/b\\/.*/)) {
-  document.querySelector(".header-search-input").value = "due:overdue -is:archived -list:don";
-  document.querySelector(".header-search-input").dispatchEvent(new FocusEvent("focus")));
+if (window.location.pathname.match(/\/b\/.*/)) {
+  document.querySelector('.header-search-input').value = 'due:overdue -is:archived -list:"turned in" -list:don';
+  document.querySelector('.header-search-input').dispatchEvent(new FocusEvent('focus'));
 } else {
-  window.open("https://trello.com/search?q=due%3Aoverdue%20-is%3Aarchived%20-list%3Adone","_self");
+  window.open('https://trello.com/search?q=due:overdue -is:archived -list:done -list:"turned in"','_blank');
 }
